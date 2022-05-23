e5878701dc536eca30185d986ffba9c7eb159951
mudittiwari <68576920+mudittiwari@users.noreply.github.com>
2022-05-23 08:47:11 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App()
 {
     return( 
     <>
-        <HashRouter basename={process.env.PUBLIC_URL}>
+        <HashRouter>
             <Routes>
                 <Route exact path="/" element={<Homepage/>}/>
                 
